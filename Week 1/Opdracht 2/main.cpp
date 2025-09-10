51b5ae4a2c095589d85b9009fb81f296ece521d8
Matthijs-V0s <s1200598@student.windesheim.nl>
2025-09-10 12:08:26 +0200

    made 'opdracht 2'

---
+++ Week 1/Opdracht 2/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main(void) {
+	int BIT0 = 0b00000001;
+	int BIT1 = 0b00000010;
+	int BIT2 = 0b00000100;
+	int BIT3 = 0b00001000;
+	int BIT4 = 0b00010000;
+	int BIT5 = 0b00100000;
+	int BIT6 = 0b01000000;
+	int BIT7 = 0b10000000;
+}
