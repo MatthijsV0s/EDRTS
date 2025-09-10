6f85f1ca4ee81d3b4126f9116ac9f9c89dfca072
Matthijs-V0s <s1200598@student.windesheim.nl>
2025-09-10 12:45:59 +0200

    added printBinary

--- Week 1/Opdracht 2/main.cpp
+++ Week 1/Opdracht 2/main.cpp
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
 
-
+void printBinary(uint8_t n);
 
 int main(void) {
-	int BIT0 = 0b00000001;
-	int BIT1 = 0b00000010;
-	int BIT2 = 0b00000100;
-	int BIT3 = 0b00001000;
-	int BIT4 = 0b00010000;
-	int BIT5 = 0b00100000;
-	int BIT6 = 0b01000000;
-	int BIT7 = 0b10000000;
+	uint8_t BIT0 = 0b00000001;
+	uint8_t BIT1 = 0b00000010;
+	uint8_t BIT2 = 0b00000100;
+	uint8_t BIT3 = 0b00001000;
+	uint8_t BIT4 = 0b00010000;
+	uint8_t BIT5 = 0b00100000;
+	uint8_t BIT6 = 0b01000000;
+	uint8_t BIT7 = 0b10000000;
+	printBinary(BIT7);
+}
+
+void printBinary(uint8_t n) {
+	for (int i = 7; i >= 0; i--) {
+		printf("%u", (n >> i) & 1);
+		}
+	printf("\n");
 }
