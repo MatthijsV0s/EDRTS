210fabcaa5ddee1175e73f3f91c8e1899d353a69
Matthijs-V0s <s1200598@student.windesheim.nl>
2025-09-10 14:11:51 +0200

    added an interest array

--- Week 1/Opdracht 3/main.cpp
+++ Week 1/Opdracht 3/main.cpp
@@ -1,37 +1,44 @@
 #include <stdio.h>
 
-void leesWaarden(float* s, int* r, int* j);
-void printTabel(float* s, int* r, int* j);
+void leesWaarden(float* geld, int rente[], int lengte, int* jaar);
+void printTabel(float* s, int r[], int lengte, int* j);
 
 int main()
 {
-    float sp;
-    int rp, jp;
+    float start;
+    int rente[100];
+    int jaren;
 
-    sp = 100;
-    rp = 1;
-    jp = 0;
-    leesWaarden(&sp, &rp, &jp);
-    printTabel(&sp, &rp, &jp);
+//    start = 100;
+//    rente = 1;
+//    jaren = 0;
+    leesWaarden(&start, rente, 100, &jaren);
+    printTabel(&start, rente, 100, &jaren);
 
 }
 
-void leesWaarden(float* geld, int* rente, int* jaar) {
+void leesWaarden(float* geld, int rente[], int lengte, int* jaar) {
     printf("Met hoeveel geld begint u te sparen?\n");
     scanf_s("%f", &*geld);
-
-    printf("Met hoeveel procent rente?\n");
-    scanf_s("%d", &*rente);
 
     printf("Hoeveel jaar wilt U sparen?\n");
     scanf_s("%d", &*jaar);
 
+    printf("Geef per jaar het rente percentage op.");
+    for (int i = 1; i <= *jaar; i++) {
+        printf("\nHet rente percentage voor jaar %d is:", i);
+        scanf_s("%d", &rente[i]);
+    }
+
+//    printf("Met hoeveel procent rente?\n");
+//    scanf_s("%d", &*rente);
+
     printf("\n\n");
 }
 
-void printTabel(float* s, int* r, int* j) {
+void printTabel(float* s, int r[], int lengte, int* j) {
     for (int i = 1; i <= *j; i++) {
-        *s = (*s * (100 + *r)) / 100;
+        *s = (*s * (100 + r[i])) / 100;
         printf("U heeft na %d jaar %.2f euro gespaard.\n", i, *s);
     }
 }
